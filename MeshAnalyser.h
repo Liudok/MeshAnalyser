4e214edd68ffc6c8f6289243ed6c7dbfa68b842c
Viacheslav Slavhorodskyi <viacheslav.slavhorodskyi@materialise.kiev.ua>
2018-07-30 17:42:46 +0300

    Widget in Window

--- MeshAnalyser.h
+++ MeshAnalyser.h
@@ -2,6 +2,14 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_MeshAnalyser.h"
+#include <QOpenGLWidget>
+#include <QMatrix4x4>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLTexture>
+#include <QOpenGLBuffer>
+#include <QMouseEvent>
+#include <QKeyEvent>
+
 
 class MeshAnalyser : public QMainWindow
 {
@@ -12,4 +20,33 @@ public:
 
 private:
 	Ui::MeshAnalyserClass ui;
+	Ui::Widget* openGLWidget;
 };
+
+class Ui::Widget : public QOpenGLWidget
+	{
+	Q_OBJECT
+
+	public:
+		explicit Widget(QWidget *parent = nullptr);
+
+	private:
+		QOpenGLTexture *m_texture;
+		QOpenGLBuffer m_indexBuffer;
+		QMatrix4x4 m_projectionMatrix;
+		QOpenGLShaderProgram m_program;
+		QOpenGLBuffer m_arrayBuffer;
+		QVector2D m_mousePosition;
+		QQuaternion m_rotation;
+
+	protected:
+		//void initializeGL();
+		//void resizeGL(int w, int h);
+		//void paintGL();
+
+		//void mousePressEvent(QMouseEvent * event);
+		//void mouseMoveEvent(QMouseEvent * event);
+
+		//void initShaders();
+		//void initMesh(float width);
+	};
