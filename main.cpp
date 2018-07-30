fcaf3b8d9190325d3d6a349e8ff406fda69f54bd
Sofiia Kolesnyk <sofiia.kolesnyk@materialise.kiev.ua>
2018-07-30 16:21:27 +0300

    import obj on start

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
 #include "MeshAnalyser.h"
 #include <QtWidgets/QApplication>
+#include <QMessageBox>
 #include "ObjParser.h"
 #include <iostream>
+#include<string>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+
+	if (argc < 2)
+	{
+		QMessageBox noObjToParseMessage;
+		noObjToParseMessage.critical(0, "Error", "No *.obj file to parse! :(");
+		noObjToParseMessage.setFixedSize(700, 200);
+		return -1;
+	}
+
 	MeshAnalyser w;
 	try {
-		Mesh ourMesh = parseObjFile("C:\\Users\\noprysk\\Source\\Repos\\MeshAnalyser2\\sphere.obj");
+		Mesh ourMesh = parseObjFile(argv[1]);
 	}
 	catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
