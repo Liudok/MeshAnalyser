d99ff6a124a58b46248ba839f617bd052591e3d6
Nestor Oprysk <nestor.oprysk@materialise.kiev.ua>
2018-07-31 15:33:37 +0300

    The first test is ready to serve!

--- Tests/Test.cpp
+++ Tests/Test.cpp
@@ -1,15 +1,26 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "../LogicLib/logic.h"
+#include <fstream>
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+std::string makeAndFillInputFile()
+{
+	std::string fileName("test1.obj");
+	std::ofstream fileStream(fileName);
+	fileStream << "v 0.038060 3.980785 -0.191342" << std::endl;
+	return fileName;
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
-	REQUIRE( mySum(5, 5) == 10);
+bool basicMeshTest1()
+{
+	const auto inputFile = makeAndFillInputFile();
+	const auto myMesh = parseObjFile(inputFile.c_str());
+	return myMesh.vertices.size() == 1 &&
+		myMesh.vertices[0] == QVector3D(std::stof("0.038060"),
+			std::stof("3.980785"), std::stof("-0.191342")) &&
+		myMesh.triangles.empty();
+}
+
+TEST_CASE( "ObjParser Basics", "[ObjParser]" ) {
+    REQUIRE(basicMeshTest1());
 }
