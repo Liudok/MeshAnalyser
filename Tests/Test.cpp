5e300af38034b8ef16c46a71d0036de261f828c1
Nestor Oprysk <nestor.oprysk@materialise.kiev.ua>
2018-07-31 17:05:30 +0300

    ObjParser is now tested

--- Tests/Test.cpp
+++ Tests/Test.cpp
@@ -3,17 +3,17 @@
 #include "../LogicLib/logic.h"
 #include <fstream>
 
-std::string makeAndFillInputFile()
+std::string makeAndFillInputFile(std::string const& fileName, std::string const& input)
 {
-	std::string fileName("test1.obj");
 	std::ofstream fileStream(fileName);
-	fileStream << "v 0.038060 3.980785 -0.191342" << std::endl;
+	fileStream << input << std::endl;
 	return fileName;
 }
 
-bool basicMeshTest1()
+bool correctlyParsesVertex()
 {
-	const auto inputFile = makeAndFillInputFile();
+	const auto inputFile = makeAndFillInputFile("test1.obj",
+		"v 0.038060 3.980785 -0.191342");
 	const auto myMesh = parseObjFile(inputFile.c_str());
 	return myMesh.vertices.size() == 1 &&
 		myMesh.vertices[0] == QVector3D(std::stof("0.038060"),
@@ -21,6 +21,41 @@ bool basicMeshTest1()
 		myMesh.triangles.empty();
 }
 
+bool correctlyParsesTriangle()
+{
+	const auto inputFile = makeAndFillInputFile("test2.obj",
+		"v 0.038060 3.980785 -0.191342\n"
+		"v 0.038060 3.980785 -0.191342\n"
+		"v 0.038060 3.980785 -0.191342\n"
+		"f 0//512 1//512 2//512");
+	const auto myMesh = parseObjFile(inputFile.c_str());
+	return myMesh.vertices.size() == 3 &&
+		myMesh.vertices[0] == QVector3D(std::stof("0.038060"),
+			std::stof("3.980785"), std::stof("-0.191342"));
+		myMesh.vertices[2] == myMesh.vertices[1] &&
+		myMesh.vertices[1] == myMesh.vertices[0];
+		myMesh.triangles.size() == 1 &&
+		myMesh.triangles[0].v1 == 0 &&
+		myMesh.triangles[1].v1 == 1 &&
+		myMesh.triangles[2].v1 == 2;
+}
+
+bool correctlyThrowsExceptionWhenTriangleRequiresNonExistentVertex()
+{
+	const auto inputFile = makeAndFillInputFile("test3.obj",
+		"f 0//512 1//512 2//512");
+	auto exceptionThrown = false;
+	try {
+		const auto myMesh = parseObjFile(inputFile.c_str());
+	}
+	catch (std::runtime_error& e) {
+		exceptionThrown = true;
+	}
+	return exceptionThrown;
+}
+
 TEST_CASE( "ObjParser Basics", "[ObjParser]" ) {
-    REQUIRE(basicMeshTest1());
+    REQUIRE(correctlyParsesVertex());
+	REQUIRE(correctlyParsesTriangle());
+	REQUIRE(correctlyThrowsExceptionWhenTriangleRequiresNonExistentVertex());
 }
